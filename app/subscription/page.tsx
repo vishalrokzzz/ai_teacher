50f77794f85230bda81c7cda24b3fac1e1d0dfdb
vishalrokzzz <vishaldasari696@gmail.com>
2025-07-01 22:38:55 +0530

    add supabase connection

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -3,7 +3,7 @@ import PricingTableClient from "../../components/PricingTableClient";
 
 const Subscription = () => {
     return (
-        <div>
+        <div className={"flex items-center justify-center py-19"}>
             <PricingTableClient />
         </div>
     );
