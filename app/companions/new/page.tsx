67e45171e6a740f2e1594a033dbd240725641988
vishalrokzzz <vishaldasari696@gmail.com>
2025-07-01 23:16:26 +0530

    create supabase client and add logic for creating a companinon

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import CompanionForm from "@/components/CompanionForm";
+import {auth} from "@clerk/nextjs/server";
+import {redirect} from "next/navigation";
 
-const NewCompanion = () => {
+const NewCompanion = async () => {
+    const {userId}= await auth();
+    if(!userId)  redirect("/sign-in");
+
+
     return (
         <main className={"max-md:w-2/3 min-lg:w-1/3 items-center justify-center"}>
             <article className={"w-full gap-4 "}>
---
+++ lib/actions/companion.action.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import {auth} from "@clerk/nextjs/server";
+import {createSupabaseClient} from "@/lib/supabase";
+
+export const createCompanion = async (formData: CreateCompanion) =>{
+    const {userId : author} = await auth();
+    const supabase = createSupabaseClient();
+
+    const {data,error} = await supabase.from("companion").insert({formData,author}).select();
+    if (error || !data) throw new Error(error?.message || "failed to crete a companion");
+
+    return data[0];
+
+
+}
