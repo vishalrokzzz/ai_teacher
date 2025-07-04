bbfe07934dc20ef755b7dc315b5fe0fc9e9f6646
vishalrokzzz <vishaldasari696@gmail.com>
2025-07-04 11:56:04 +0530

    implent functional data in home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,42 +3,41 @@ import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import Cta from "@/components/Cta";
 import {recentSessions} from "@/constants";
+import {getALLCompanions, getRecentSessions} from "@/lib/actions/companion.action";
+import {asyncWalk} from "estree-walker";
+import {getSubjectColor} from "@/lib/utils";
 
-const Page = () => {
+const Page = async () => {
+
+
+    const companions=await getALLCompanions({limit: 3});
+    const recentSessionCompanions=await getRecentSessions();
+
+
   return (
     <main>
       <h1 className={"p-1"}>Popular companions</h1>
+
+
+
         <section className={"home-section"}>
-            <CompanionCard
-            id="1"
-            name={"ai philosopher"}
-            duration={45}
-            color={"#acf357"}
-            subject={"philosophy"}
-            topic={"existentialism"}
-            />
-            <CompanionCard
-                id="2"
-                name={"ai writer"}
-                duration={46}
-                color={"#e1d0fa"}
-                subject={"philosophy"}
-                topic={"existentialism"}
-            />
-            <CompanionCard
-                id="3"
-                name={"ai boxer"}
-                duration={47}
-                color={"#c6e5fc"}
-                subject={"philosophy"}
-                topic={"existentialism"}
-            />
+            {
+                companions?.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))
+            }
+
+
         </section>
 
         <section className={"home-section"}>
             <CompanionsList
                 title={"recent companions"}
-                companions={recentSessions}
+                companions={recentSessionCompanions}
                 classNames={"w-2/3 max-lg:w-full"}
             />
             <Cta/>
